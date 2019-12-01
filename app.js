7a36ebd3b5ea829728836c4cb907c4fdee9c3da3
Josh Frechette <josh.f.design@gmail.com>
2019-11-30 23:35:59 -0500

    Reporestarted due to irrevocable damage caused by file manipulation done directly on Github. Program structure and app.js and employee.js updated.

---
+++ app.js
@@ -0,0 +1,14 @@
+const Employee = require('./employee');
+
+
+async function classDir(userInfo) {
+    console.log(employeeInfo);
+    console.log(userData.name)
+    if (userData.title === "manager"){
+
+    }
+}
+
+
+init()
+
